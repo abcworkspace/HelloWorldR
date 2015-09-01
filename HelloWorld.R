27eb9d2458dd27a5693c1f88e6b51df26e8af01c
abcworkspace <abcworkspace@gmail.com>
2015-09-01 01:12:14 -0400

    ok

--- HelloWorld.R
+++ HelloWorld.R
@@ -3,7 +3,7 @@
 # Author: Vu Huynh
 ###############################################################################
 
-print("Hello World in R. Now can do this quickly\n")
+print("Hello World in R. Now can do this quickly. Change from R Stutio\n")
 a = 2
 b = 3
 c = a + b
