a5202ae811509a5c0f8d2a113eea44f56ee9cf53
abcworkspace <abcworkspace@users.noreply.github.com>
2015-09-01 01:09:48 -0400

    Update HelloWorld.R
    change

--- HelloWorld.R
+++ HelloWorld.R
@@ -3,7 +3,7 @@
 # Author: Vu Huynh
 ###############################################################################
 
-print("Hello World in R\n")
+print("Hello World in R. Now can do this quickly\n")
 a = 2
 b = 3
 c = a + b
